763aa6d7c01685f5ddf09d430a0fa0cc9e00a1c9
pratikgupta142000 <1941012585.q.pratikgupta@gmail.com>
2025-03-04 16:48:20 +0530

    minor changes done

---
+++ diabetes-prediction/src/App.js
@@ -0,0 +1,13 @@
+// ./src/App.js
+import React from 'react';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
